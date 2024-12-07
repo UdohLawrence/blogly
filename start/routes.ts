fcdc7e4d01892013b9f521b95dfe4b63accd8a2b
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-12-07 08:21:04 +0100

    add aliases for the regiter routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,8 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+const RegisterController = () => import('#controllers/auth/register_controller')
 
 router.on('/').render('pages/home')
+router.group(() => {
+  router.get('/register', [RegisterController, 'show']).as('register.show')
+  router.post('/register', [RegisterController, 'store']).as('register.store')
+}).prefix('/auth').as('auth')
